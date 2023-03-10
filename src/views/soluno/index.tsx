29c0b3bdf50f54151590968cd5a0b4e1554c6ed7
etherGenius <elias.tech.dev@gmail.com>
2023-03-10 15:08:31 +0300

    Added soluno view

---
+++ src/views/soluno/index.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react"
+import { FetchRandomNft } from "../../components/FetchRandomNft"
+
+
+
+export const SolUnoView: FC = ({ }) => {
+    return (
+       
+        <div className="md:hero mx-auto p-4">
+          <div className="md:hero-content flex flex-col">
+            <h1 className="text-center text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
+              Explore Solana Nfts With Rarible
+            </h1>
+            
+            {/* CONTENT GOES HERE */}
+            <div className="text-center">
+              <FetchRandomNft />
+            </div>
+          </div>
+        </div>
+    )
+}
