1027ad107fbb951deb8b84a108e75af2c170d0ec
etherGenius <elias.tech.dev@gmail.com>
2023-03-12 09:19:43 +0300

    Added fetch nft

--- src/components/GetRandomNft.tsx
+++ src/components/GetRandomNft.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+
+//  https://api.rarible.org/v0.1/items/byOwner/?owner=ETHEREUM:0x4765273c477c2dc484da4f1984639e943adccfeb
 export default function GetRandomNft() {
   return (
     <div>GetRandomNft</div>
