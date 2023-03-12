17d48a694dc8951626ea3f4e8c5e3e56165ce170
etherGenius <elias.tech.dev@gmail.com>
2023-03-12 09:42:12 +0300

    Added soluno

--- src/components/GetRandomNft.tsx
+++ src/components/GetRandomNft.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useState } from "react";
 
+const [data, setData] = useState([]);
+const getNftData = () => {
+    const options = { method: "GET", headers: { accept: "application/json" } };
+    // https://api.opensea.io/api/v1/assets
+    // https://api.rarible.org/v0.1/items/byOwner/?owner=ETHEREUM:0x4765273c477c2dc484da4f1984639e943adccfeb
+    fetch("https://api.opensea.io/api/v1/assets", options)
+        .then((response) => response.json())
+        .then((response) => {
+            setData(response.assets);
+            console.log(response);
+        })
+        .catch((err) => console.error(err));
+};
 
 //  https://api.rarible.org/v0.1/items/byOwner/?owner=ETHEREUM:0x4765273c477c2dc484da4f1984639e943adccfeb
 export default function GetRandomNft() {
-  return (
-    <div>GetRandomNft</div>
-  )
+    return (
+        <div className="secondP">
+            {data.map((nft, index) => {
+                return (
+                    <div key={index}>
+                        <img src={nft.image_url} alt={"nft.im"} />
+                        <p>{nft.name}</p>
+                        <p>{nft.id}</p>
+                        <p>{nft.asset_contract.opensea_seller_fee_basis_points}</p>
+                    </div>
+                );
+            })}
+        </div>
+    )
 }
--- src/components/SolunoGame.tsx
+++ src/components/SolunoGame.tsx
@@ -5,8 +5,6 @@ import Link from "next/link"
 function SolunoGame() {
     return (
         <div className="mt-40 ">
-            {/* 
-            <Heading title={"Play SolUNO"} /> */}
             <div className="container mx-auto mt-1">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-1">
                     <div className="flex justify-center text-6xl  p-4 ">
@@ -23,14 +21,22 @@ function SolunoGame() {
                                 Random UNO
                             </button>
                         </Link>
-
-
-
                     </div>
                     <div className="flex justify-center text-6xl  p-4 ">
 
-                        lkjhg
+                        <Link
+                            href={{
+                                pathname: '/explore',
+                                query: { name: 'explore' },
+                            }}
+                        >
+                            <button
+                                className="px-12 m-2 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
 
+                            >
+                                Match UNO
+                            </button>
+                        </Link>
                     </div>
 
                 </div>
